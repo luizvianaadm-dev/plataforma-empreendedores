6ec5351813d2e2133c8c349eed9ec10d23253139
luizvianaadm-dev <luizviana.adm@gmail.com>
2025-11-08 04:28:04 -0300

    Fix: Add missing 'i' to import statement in mei.tsx line 1

--- pages/mei.tsx
+++ pages/mei.tsx
@@ -1,4 +1,4 @@
-mport { useState } from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import { supabase } from '../lib/supabaseClient'
 import { atividadesMEI, categorias } from '../lib/cnaeData'
