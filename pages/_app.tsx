3e29de5ee9ef8554536ae635ac9b3224766e6c31
luizvianaadm-dev <luizviana.adm@gmail.com>
2025-11-12 10:27:55 -0300

    Fix: Remove next-auth SessionProvider import from _app.tsx
    - Removed unused SessionProvider import from next-auth/react
    - SessionProvider was not being used in the component
    - This was causing TypeScript compilation error during build
    - Fixes: Cannot find module 'next-auth/react' error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,18 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import { SessionProvider } from 'next-auth/react'
 import { OrganizationProvider } from '../lib/contexts/OrganizationContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-
-          <OrganizationProvider>
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </div>
+    <OrganizationProvider>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </OrganizationProvider>
   )
 }
