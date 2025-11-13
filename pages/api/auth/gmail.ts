030afb22c052a96d785a8979649a2676e02e5981
luizvianaadm-dev <luizviana.adm@gmail.com>
2025-11-12 23:58:34 -0300

    Update gmail.ts

--- pages/api/auth/gmail.ts
+++ pages/api/auth/gmail.ts
@@ -1,4 +1,4 @@
-gmail.tsimport { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,10 +13,10 @@ export default async function handler(
     const clientId = process.env.GOOGLE_CLIENT_ID;
     const redirectUri = `${process.env.NEXTAUTH_URL}/api/auth/gmail/callback`;
     const scope = encodeURIComponent('https://www.googleapis.com/auth/gmail.send https://www.googleapis.com/auth/gmail.readonly');
-    
+
     const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`;
-    
-    return res.status(200).json({ 
+
+    return res.status(200).json({
       success: true,
       authUrl,
       message: 'Gmail OAuth iniciado com sucesso'
